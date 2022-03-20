b4e6afa836feaef247f61dc21fa996494d91362b
rivsidn <rivsidn@163.com>
2022-03-20 22:54:30 +0800

    2022-03-20 22:54:30

---
+++ python/test/in.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+txt = "China is a great country"
+
+x = "ina" in txt
+
+print(x)
+
---
+++ python/test/string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+niaho
+'''
+
+"""
+nihao
+"""
+
+str="abcdef"
+
+print(str[0])
+print(str[0:2])
+print(str[2:-1])
+
